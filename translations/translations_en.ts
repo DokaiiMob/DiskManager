3513028caa0df86ff0ea1a5b50d97beba084e80e
XandriXoX <66661480+DokaiiMob@users.noreply.github.com>
2024-11-12 20:03:26 +0700

    Add files via upload

---
+++ translations/translations_en.ts
@@ -0,0 +1,339 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name></name>
+    <message>
+        <location filename="../main.py" line="45"/>
+        <location filename="../main.py" line="684"/>
+        <source>Refresh Disk Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>Disk</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>Mount Point</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>FS</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>Total (GB)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="55"/>
+        <source>Used (GB)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="55"/>
+        <source>Free (GB)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="55"/>
+        <source>Usage (%)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="56"/>
+        <source>Type</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="83"/>
+        <source>Removable</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="84"/>
+        <source>Internal</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="98"/>
+        <source>Used</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="99"/>
+        <source>Free</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="122"/>
+        <source>No Access</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="140"/>
+        <location filename="../main.py" line="274"/>
+        <source>Search:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="162"/>
+        <source>Select All</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="165"/>
+        <source>Deselect All</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="176"/>
+        <source>Encrypt Archives</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="177"/>
+        <source>Incremental Backup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="178"/>
+        <source>Archive Format:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="184"/>
+        <location filename="../main.py" line="688"/>
+        <source>Start Backup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="225"/>
+        <location filename="../main.py" line="254"/>
+        <source>Backup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="225"/>
+        <source>Please select at least one folder for backup.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="229"/>
+        <source>Choose Backup Destination</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="254"/>
+        <source>Backup Completed</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="282"/>
+        <source>Analyze Disk Space</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="293"/>
+        <location filename="../main.py" line="692"/>
+        <source>Start Cleanup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="310"/>
+        <source>Filter by File Types</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="318"/>
+        <source>Delete Large Files (&gt;)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="336"/>
+        <source>Analyzing temporary files...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="358"/>
+        <location filename="../main.py" line="394"/>
+        <location filename="../main.py" line="400"/>
+        <location filename="../main.py" line="424"/>
+        <source>Cleanup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="358"/>
+        <source>Invalid size format for large files.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="394"/>
+        <source>Please select files to delete.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="401"/>
+        <source>Are you sure you want to delete {} files?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="424"/>
+        <source>Cleanup Completed</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="446"/>
+        <location filename="../main.py" line="456"/>
+        <source>CPU Usage:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="447"/>
+        <location filename="../main.py" line="461"/>
+        <source>Memory Usage:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="448"/>
+        <location filename="../main.py" line="464"/>
+        <source>Network Usage:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="516"/>
+        <location filename="../main.py" line="668"/>
+        <location filename="../main.py" line="669"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="525"/>
+        <source>Choose Theme:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="529"/>
+        <source>Light</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="529"/>
+        <location filename="../main.py" line="709"/>
+        <source>Dark</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="533"/>
+        <source>Choose Language:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="542"/>
+        <source>Save</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="544"/>
+        <source>Cancel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="551"/>
+        <location filename="../main.py" line="596"/>
+        <source>Export Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="553"/>
+        <location filename="../main.py" line="606"/>
+        <source>Import Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="601"/>
+        <source>Export</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="601"/>
+        <source>Settings exported successfully.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="612"/>
+        <source>Import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="612"/>
+        <source>Settings imported successfully.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="620"/>
+        <location filename="../main.py" line="741"/>
+        <source>Disk Management and Backup Tool</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="634"/>
+        <location filename="../main.py" line="742"/>
+        <source>Disk Information</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="635"/>
+        <location filename="../main.py" line="743"/>
+        <source>Disk Backup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="636"/>
+        <location filename="../main.py" line="744"/>
+        <source>Disk Cleanup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="637"/>
+        <location filename="../main.py" line="745"/>
+        <source>System Information</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="647"/>
+        <source>Ready</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="660"/>
+        <source>File</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="661"/>
+        <source>Exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="675"/>
+        <source>Help</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="676"/>
+        <location filename="../main.py" line="770"/>
+        <source>About</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="771"/>
+        <source>About Program</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/translations_ru.ts
@@ -0,0 +1,339 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name></name>
+    <message>
+        <location filename="../main.py" line="45"/>
+        <location filename="../main.py" line="684"/>
+        <source>Refresh Disk Info</source>
+        <translation>Обновить информацию о диске</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>Disk</source>
+        <translation>Диск</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>Mount Point</source>
+        <translation>Название диска</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>FS</source>
+        <translation>ФС</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="54"/>
+        <source>Total (GB)</source>
+        <translation>Всего (ГБ)</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="55"/>
+        <source>Used (GB)</source>
+        <translation>Использовано (ГБ)</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="55"/>
+        <source>Free (GB)</source>
+        <translation>Свободно (ГБ)</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="55"/>
+        <source>Usage (%)</source>
+        <translation>Использовано (%)</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="56"/>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="83"/>
+        <source>Removable</source>
+        <translation>Съемный</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="84"/>
+        <source>Internal</source>
+        <translation>Внутренний</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="98"/>
+        <source>Used</source>
+        <translation>Используется</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="99"/>
+        <source>Free</source>
+        <translation>Свободно</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="122"/>
+        <source>No Access</source>
+        <translation>Нет доступа</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="140"/>
+        <location filename="../main.py" line="274"/>
+        <source>Search:</source>
+        <translation>Поиск:</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="162"/>
+        <source>Select All</source>
+        <translation>Выбрать всё</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="165"/>
+        <source>Deselect All</source>
+        <translation>Отменить выбор всех файлов</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="176"/>
+        <source>Encrypt Archives</source>
+        <translation>Зашифровать архивы</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="177"/>
+        <source>Incremental Backup</source>
+        <translation>Инкрементное резервное копирование</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="178"/>
+        <source>Archive Format:</source>
+        <translation>Архивный формат:</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="184"/>
+        <location filename="../main.py" line="688"/>
+        <source>Start Backup</source>
+        <translation>Начать резервное копирование</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="225"/>
+        <location filename="../main.py" line="254"/>
+        <source>Backup</source>
+        <translation>Резервное копирование</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="225"/>
+        <source>Please select at least one folder for backup.</source>
+        <translation>Выберите хотя бы одну папку для резервного копирования.</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="229"/>
+        <source>Choose Backup Destination</source>
+        <translation>Выберите место назначения резервного копирования</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="254"/>
+        <source>Backup Completed</source>
+        <translation>Резервное копирование завершено</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="282"/>
+        <source>Analyze Disk Space</source>
+        <translation>Анализ дискового пространства</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="293"/>
+        <location filename="../main.py" line="692"/>
+        <source>Start Cleanup</source>
+        <translation>Начать очистку</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="310"/>
+        <source>Filter by File Types</source>
+        <translation>Фильтр по типам файлов</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="318"/>
+        <source>Delete Large Files (&gt;)</source>
+        <translation>Удаление больших файлов (&gt;)</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="336"/>
+        <source>Analyzing temporary files...</source>
+        <translation>Анализ временных файлов...</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="358"/>
+        <location filename="../main.py" line="394"/>
+        <location filename="../main.py" line="400"/>
+        <location filename="../main.py" line="424"/>
+        <source>Cleanup</source>
+        <translation>Очистка</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="358"/>
+        <source>Invalid size format for large files.</source>
+        <translation>Неверный формат размера для больших файлов.</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="394"/>
+        <source>Please select files to delete.</source>
+        <translation>Выберите файлы для удаления.</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="401"/>
+        <source>Are you sure you want to delete {} files?</source>
+        <translation>Вы уверены, что хотите удалить {} файлов?</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="424"/>
+        <source>Cleanup Completed</source>
+        <translation>Очистка завершена</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="446"/>
+        <location filename="../main.py" line="456"/>
+        <source>CPU Usage:</source>
+        <translation>Использование процессора:</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="447"/>
+        <location filename="../main.py" line="461"/>
+        <source>Memory Usage:</source>
+        <translation>Памяти использовано:</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="448"/>
+        <location filename="../main.py" line="464"/>
+        <source>Network Usage:</source>
+        <translation>Использовано  сети:</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="516"/>
+        <location filename="../main.py" line="668"/>
+        <location filename="../main.py" line="669"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="525"/>
+        <source>Choose Theme:</source>
+        <translation>Выберите тему:</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="529"/>
+        <source>Light</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="529"/>
+        <location filename="../main.py" line="709"/>
+        <source>Dark</source>
+        <translation>Тёмная</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="533"/>
+        <source>Choose Language:</source>
+        <translation>Выберите язык:</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="542"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="544"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="551"/>
+        <location filename="../main.py" line="596"/>
+        <source>Export Settings</source>
+        <translation>Экспорт настроек</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="553"/>
+        <location filename="../main.py" line="606"/>
+        <source>Import Settings</source>
+        <translation>Импорт настроек</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="601"/>
+        <source>Export</source>
+        <translation>Экспорт</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="601"/>
+        <source>Settings exported successfully.</source>
+        <translation>Настройки успешно экспортированы.</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="612"/>
+        <source>Import</source>
+        <translation>Импорт</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="612"/>
+        <source>Settings imported successfully.</source>
+        <translation>Настройки успешно импортированы.</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="620"/>
+        <location filename="../main.py" line="741"/>
+        <source>Disk Management and Backup Tool</source>
+        <translation>Инструмент для управления дисками и резервного копирования</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="634"/>
+        <location filename="../main.py" line="742"/>
+        <source>Disk Information</source>
+        <translation>Информация о диске</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="635"/>
+        <location filename="../main.py" line="743"/>
+        <source>Disk Backup</source>
+        <translation>Резервное копирование диска</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="636"/>
+        <location filename="../main.py" line="744"/>
+        <source>Disk Cleanup</source>
+        <translation>Очистка диска</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="637"/>
+        <location filename="../main.py" line="745"/>
+        <source>System Information</source>
+        <translation>Информация о системе</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="647"/>
+        <source>Ready</source>
+        <translation>Готово</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="660"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="661"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="675"/>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="676"/>
+        <location filename="../main.py" line="770"/>
+        <source>About</source>
+        <translation>О нас</translation>
+    </message>
+    <message>
+        <location filename="../main.py" line="771"/>
+        <source>About Program</source>
+        <translation>О программе</translation>
+    </message>
+</context>
+</TS>
